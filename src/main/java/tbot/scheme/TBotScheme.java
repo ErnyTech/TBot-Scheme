b321fa5432c436247547cfae104b636a0829ab4d
Ernesto Castellotti <erny.castell@gmail.com>
2018-12-07 23:47:52 +0100

    Fix parameters parsing with new html page

--- src/main/java/tbot/scheme/TBotScheme.java
+++ src/main/java/tbot/scheme/TBotScheme.java
@@ -191,11 +191,13 @@ public class TBotScheme {
     private ParametersWrapper getParameters(Element table) {
         var parameters = new ParametersWrapper();
         var tbody = table.getElementsByTag("tbody").get(0);
+        var thead = table.getElementsByTag("thead").get(0);
         var trs = tbody.getElementsByTag("tr");
-        var requiredColumn = trs.get(0).getElementsByTag("td").get(2).text();
+        var theadTr = thead.getElementsByTag("tr").get(0);
+        var requiredColumn = theadTr.getElementsByTag("th").get(2).text();
         var alwaysRequired = !requiredColumn.equals("Required");
 
-        for (int i = 1; i < trs.size(); i++) {
+        for (int i = 0; i < trs.size(); i++) {
             var tr = trs.get(i);
             var tds = tr.getElementsByTag("td");
             var name = tds.get(0).text();
